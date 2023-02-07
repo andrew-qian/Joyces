cd025d5182b95b738eed17ff813765fb0fd62b7c
andrew-qian <andrewyukaiqian@gmail.com>
2023-02-06 21:39:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chromedriver = "./chromedriver.exe"
+    chromedriver = "./chromedriver"
     driver = webdriver.Chrome(service = Service(chromedriver), chrome_options = chrome_options)
     # chromedriver = ChromeDriverManager().install()
 
