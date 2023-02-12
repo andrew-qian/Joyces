9478db5ec510766cb651166c2b7efe88c8b813a0
andrew-qian <andrewyukaiqian@gmail.com>
2023-02-12 11:40:39 -0600

    chromedriver

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-SENDER_EMAIL = os.environ['SENDER_EMAIL']
-SENDER_PASSWORD = os.environ['SENDER_PASSWORD']
-RECEIVER_EMAILS = os.environ['RECEIVER_EMAILS']
-
-print("Starting program...")
-print("Env vars:", SENDER_EMAIL, SENDER_PASSWORD, RECEIVER_EMAILS)
